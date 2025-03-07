2a2d2f6a66c061bf68a1e46f93a5ab8eb744c510
sreeragdevops <ragsreerag26@gmail.com>
2025-03-07 16:20:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,43 @@
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+pipeline {
+    agent any
+    parameters {
+        choice(name: 'TERRAFORM_ACTION', choices: ['apply', 'destroy', 'plan'], description: 'Select Terraform action to perform')
+        string(name: 'USER_NAME', defaultValue: 'Sreerag', description: 'Specify who is running the code')
+    }
+
+
+    
+    environment {
+        AWS_ACCESS_KEY_ID = credentials('aws-jenkins-integration')
+    }
+    stages {
+        stage('Code checkout from Git') {
+            steps {
+                checkout([
+                    $class: 'GitSCM',
+                    branches: [[name: '*/main']],
+                    extensions: [],
+                    userRemoteConfigs: [[
+                        credentialsId: 'jenkins-git-integration',
+                        url: 'https://github.com/sreeragdevops/jenkins-terraform'
+                    ]]
+                ])
+            }
+        }
+
+        stage('terraform init') {
+            steps {
+                script {
+                    sh "terraform init"
+                }
+            }
+        }
+        stage('terraform apply') {
+            steps {
+                script {
+                    sh "terraform apply --auto-approve"
+                }
+            }
+        }
+    }
 }
