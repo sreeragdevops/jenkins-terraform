e50a33628fb7beed566a59aee2433f88fb2da524
sreeragdevops <ragsreerag26@gmail.com>
2025-03-07 16:29:46 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,43 +1,87 @@
-pipeline {
-    agent any
-    parameters {
-        choice(name: 'TERRAFORM_ACTION', choices: ['apply', 'destroy', 'plan'], description: 'Select Terraform action to perform')
-        string(name: 'USER_NAME', defaultValue: 'Sreerag', description: 'Specify who is running the code')
-    }
+# IAM Role for the EKS Cluster
+resource "aws_iam_role" "cluster-role" {
+  name = "cluster-role"
 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
 
-    
-    environment {
-        AWS_ACCESS_KEY_ID = credentials('aws-jenkins-integration')
-    }
-    stages {
-        stage('Code checkout from Git') {
-            steps {
-                checkout([
-                    $class: 'GitSCM',
-                    branches: [[name: '*/main']],
-                    extensions: [],
-                    userRemoteConfigs: [[
-                        credentialsId: 'jenkins-git-integration',
-                        url: 'https://github.com/sreeragdevops/jenkins-terraform'
-                    ]]
-                ])
-            }
-        }
+# Attach necessary policies to the cluster role
+resource "aws_iam_role_policy_attachment" "cluster-policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster-role.name
+}
 
-        stage('terraform init') {
-            steps {
-                script {
-                    sh "terraform init"
-                }
-            }
+# IAM Role for the EKS Node Group
+resource "aws_iam_role" "node-role" {
+  name = "node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
         }
-        stage('terraform apply') {
-            steps {
-                script {
-                    sh "terraform apply --auto-approve"
-                }
-            }
-        }
-    }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Attach necessary policies to the node role
+resource "aws_iam_role_policy_attachment" "node-policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.node-role.name
+}
+
+resource "aws_iam_role_policy_attachment" "cni-policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"  # Updated policy ARN
+  role       = aws_iam_role.node-role.name
+}
+
+resource "aws_iam_role_policy_attachment" "registry-policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.node-role.name
+}
+
+# EKS Cluster
+resource "aws_eks_cluster" "eks-cluster" {
+  name     = "k8-cluster"
+  role_arn = aws_iam_role.cluster-role.arn
+  version  = "1.31"
+
+  vpc_config {
+    subnet_ids         = ["subnet-0cc732b02cc5edbab" , "subnet-019c39afd157d5141"]
+    security_group_ids = ["sg-01aad0345607711f0"]
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.cluster-policy]
+}
+
+# EKS Node Group
+resource "aws_eks_node_group" "k8-cluster-node-group" {
+  cluster_name    = aws_eks_cluster.eks-cluster.name
+  node_group_name = "k8-cluster-node-group"
+  node_role_arn   = aws_iam_role.node-role.arn
+  subnet_ids      = ["subnet-0cc732b02cc5edbab" ,"subnet-019c39afd157d5141"]
+
+  scaling_config {
+    desired_size = 3
+    min_size     = 2
+    max_size     = 5
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.node-policy]
 }
